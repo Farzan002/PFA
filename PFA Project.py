c57829f6f098fba0b3fc224dcb9de64c604e7e19
Farzan Hussain <113101934+Farzan002@users.noreply.github.com>
2022-12-06 16:32:46 +0530

    Add files via upload

---
+++ PFA Project.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    c = a+b
+    print(c)
+def sub(a,b):
+    c = a-b
+    print(c)
+def mul(a,b):
+    c = a*b
+    print(c)
+def div(a,b):
+    c = a/b
+    print(c)
+def mod(a,b):
+    c = a%b
+    print(c)
+
+x = int(input("Enter First Number"))
+y = int(input("Enter Seconde Number"))
+add(x,y)
+sub(x,y)
+mul(x,y)
+div(x,y)
+mod(x,y)
